7eeb84d61c576595c141097f021147bdc9444017
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2022-03-18 18:41:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Status = "";
+userObject = "";
+function setup(){
+    canvas = createCanvas(600, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 600, 400);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    userObject = document.getElementById("input").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded");
+    Status = true;
+}
